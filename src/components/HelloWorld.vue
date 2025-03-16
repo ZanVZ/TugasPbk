da00f0898bafa26969e188db8aab6f146ceaa234
ZanVZ <Robiihzanpermana@student.uir.ac.id>
2025-03-17 00:31:49 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>Nama: {{ nama }}</p>
+    <button @click="ubahNama">Ubah Nama</button>
+    
+    <p v-if="isOnline">User Sedang Online</p>
+    <p v-else>User Sedang Offline</p>
+    <button @click="toggleStatus">Ganti Status</button>
+    
+    <input v-model="nama" placeholder="Masukkan nama" />
+    <p>Nama yang Anda ketik: {{ nama }}</p>
+    
+    <button @click="sayHello">Klik Aku</button>
+    
+    <p :style="{ color: warna }">Teks Berwarna</p>
+    <button @click="gantiWarna">Ganti Warna</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const nama = ref('Budi');
+const isOnline = ref(true);
+const warna = ref('blue');
+
+const ubahNama = () => {
+  nama.value = 'Andi';
+};
+
+const toggleStatus = () => {
+  isOnline.value = !isOnline.value;
+};
+
+const sayHello = () => {
+  alert('Halo, selamat datang di Vue.js!');
+};
+
+const gantiWarna = () => {
+  warna.value = warna.value === 'blue' ? 'red' : 'blue';
+};
+</script>
